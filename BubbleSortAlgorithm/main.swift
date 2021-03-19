888f255f0e74b3b10aa5e3455e8956a2c3e40ade
Ilana Mohamed <Imohamed@lcs.on.ca>
2021-03-19 14:32:59 -0400

    Completed basic implementation for bubble sort

--- BubbleSortAlgorithm/main.swift
+++ BubbleSortAlgorithm/main.swift
@@ -7,5 +7,57 @@
 
 import Foundation
 
-print("Hello, World!")
+//Pause to allow the user to see output
+func waitForInput() {
+    print("Press RETURN to continue")
+    _ = readLine()
+}
 
+//Create an empty array
+var dataSet: [Int] = []
+
+//populate the array
+for _ in 1...10 {
+    dataSet.append(Int.random(in: 1...100))
+}
+
+//Print the unsorted array
+print("Unsorted")
+print(dataSet)
+waitForInput()
+
+//Sort the array
+
+//Loop through the array "n"
+
+//(however many times there are elements in the array)
+for i in 0..<dataSet.count {
+    
+    // One pass through the array to float the highest number to the end
+    for j in 0..<dataSet.count - 1 {
+
+        //compare the left value to the right value
+        if dataSet[j] > dataSet[j + 1] {
+            
+            // Swap the values when left value is greater than right value
+            let temporaryValue = dataSet[j]     //Set aside the left value
+            dataSet[j] = dataSet [j + 1]        //Replace left with right
+            dataSet[j + 1] = temporaryValue     //Replace right with temporary value
+            
+        }
+        
+    }
+    //Print the array after "n" passes
+    print("Array after pass")
+    print(dataSet)
+    waitForInput()
+
+}
+
+
+
+
+//Print the sorted array
+print("sorted")
+print(dataSet)
+waitForInput()
